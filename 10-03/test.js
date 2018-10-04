f7e2d0e662cf82b84131d2ebc1ada85b02e4c797
kathrine <55874001@qq.com>
2018-10-04 12:05:03 -0500

    update can find "fine" by "ne" but can't do by "ie"

--- 10-03/test.js
+++ 10-03/test.js
@@ -1,46 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 3000
-//www.w3cschool.cn
 
 var arr = ['how', 'are', 'you', 'I', 'am', 'fine', 'thanks'];
 var arr1 = [];
 
-// console.log(arr.length);
-
-// function findFunc(input) {    
-//     let i = 0; //i=0,
-//     for( ; i<=arr[arr.length]; i++){
-//         if(arr[i].indexOf(input) != -1){
-//             arr1.push(arr[i]);
-//         }
-//         continue;
-//     }
-//     // console.log(arr[i]);
-//     // console.log(arr1);
-//     // return JSON.stringify(arr1);
-//     return arr1;
-// }
 function findFunc(input) { 
     for(let i=0 ; i<arr.length; i++){
         if(arr[i].indexOf(input) != -1){
-            const currenRow = arr[i];
-            arr1.push(currenRow);
-            // Array.prototype.push.apply(arr1, arr[i]);
+            // const currenRow = arr[i];
+            arr1.push(arr[i]);
+            return arr1;
         }
-        continue;
     }
-    return arr1;
+    return null;
 }
-// arr1.push(1);
 
 app.get('/', function(req, res) {
     var inputFromWeb = req.query.s;
-    // foundResult = findFunc(inputFromWeb);
+    arr1 = [];
     arr1 = findFunc(inputFromWeb);
-    // arr1.push(foundResult); 
-    // res.send(foundResult);
-    res.send(arr1);
+    if (arr1 != null){
+        res.send(arr1);
+    }
+    else{
+        res.send('error');
+    }
 })
 
 app.listen(3000)
+
+//还不能实现字母乱序输入时找出符合的结果
