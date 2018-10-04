3114ca0295a49ab9d3dd095f8ccf95d80b503588
kathrine <55874001@qq.com>
2018-10-04 13:03:02 -0500

    final one...find "fine" by "ne" not "ie"

--- 10-03/test1.js
+++ 10-03/test1.js
@@ -1,8 +1,34 @@
-var arr = ['parsnip', 'potato'];
-var arr1 = ['celery', 'beetroot'];
+const express = require('express')
+const app = express()
+const port = 3000
 
-// Merge the second array into the first one
-// Equivalent to vegetables.push('celery', 'beetroot');
-Array.prototype.push.apply(arr1, arr[0]);
+var arr = ['how', 'are', 'you', 'I', 'am', 'fine', 'thanks'];
+var arr1 = [];
 
-console.log(arr1);
+function findFunc(input) { 
+    for(let i=0 ; i<arr.length; i++){
+        if(arr[i].indexOf(input) != -1){
+            arr1.push(arr[i]);
+        }
+    }
+    if(arr1 != []){
+        return arr1;
+    }
+    else{
+        return null;
+    }
+}
+
+app.get('/', function(req, res) {
+    var inputFromWeb = req.query.s;
+    arr1 = [];
+    arr1 = findFunc(inputFromWeb);
+    if (arr1 != null){
+        res.send(arr1);
+    }
+    else{
+        res.send('error');
+    }
+})
+
+app.listen(3000)
