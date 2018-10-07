08247dd33751c863c581b24a28200ab638205614
kathrine <55874001@qq.com>
2018-10-07 02:29:33 -0500

    Create Files

---
+++ 10-06/Create Files.md
@@ -0,0 +1,21 @@
+# Create Files
+* fs.appendFile(), 将指定的内容附加到文件。如果文件不存在，将创建该文件
+* fs.open(), 将“flag”作为第二个参数，如果标志为“w”表示“写入”，则打开指定的文件进行写入。如果该文件不存在，则创建一个空文件
+
+>code
+
+	var fs = require('fs');
+	
+	fs.open('mynewfile2.txt', 'w', function (err, file) {
+	  if (err) throw err;
+	  console.log('Saved!');
+	});
+		
+	// fs.open('mynewfile2.txt', 'r', function (err, file) { //此处的"r"应该为读取
+	//   if (err) throw err;
+	//   console.log('Saved!');
+	// });
+
+* fs.writeFile(), 用于替换指定的文件和内容（如果存在）。如果该文件不存在，将创建包含指定内容的新文件
+
+	###所以fs.open()和fs.writeFile()是自带判断的，先判断 再执行
--- 10-06/test.js
+++ 10-06/test.js
@@ -62,6 +62,8 @@
 
 
 // Create Files
+
+// fs.appendFile(), 将指定的内容附加到文件。如果文件不存在，将创建该文件
 // var fs = require('fs');
 
 // fs.appendFile('mynewfile1.txt', 'Hello Kathy!', function (err) {
@@ -69,9 +71,45 @@
 //   console.log('Saved!');
 // });
 
-// demo for appendFile()
+// demo for fs.appendFile()
+// var fs = require('fs');
+// fs.appendFile('newText.txt', 'I am Kathy!',function(err) {
+//   if (err) throw err;
+//   console.log('Bingo!');
+// });
+
+// fs.open(), 将“flag”作为第二个参数，如果标志为“w”表示“写入”，则打开指定的文件进行写入。如果该文件不存在，则创建一个空文件
+// what is "flag"?
+// var fs = require('fs');
+
+// fs.open('mynewfile2.txt', 'w', function (err, file) {
+//   if (err) throw err;
+//   console.log('Saved!');
+// });
+
+// fs.open('mynewfile2.txt', 'r', function (err, file) {
+//   if (err) throw err;
+//   console.log('Saved!');
+// });
+
+// demo for fs.open()
+// var fs = require('fs');
+// fs.open('newText1.txt', 'w', function(err) {
+//   if (err) throw err;
+//   console.log('Bingo!');
+// });
+
+// fs.writeFile()用于替换指定的文件和内容（如果存在）。如果该文件不存在，将创建包含指定内容的新文件
+// var fs = require('fs');
+
+// fs.writeFile('mynewfile3.txt', 'Hello Moonpie!', function (err) {
+//   if (err) throw err;
+//   console.log('Saved!');
+// });
+
+// demo for fs.writeFile()
 var fs = require('fs');
-fs.appendFile('newText.txt', 'I am Kathy!',function(err) {
+fs.writeFile('newText2.txt', 'Hello Jim!', function(err) {
   if (err) throw err;
   console.log('Bingo!');
 });
