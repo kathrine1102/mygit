338959a33a5824c478542ccaea2699e4ffa40543
kathrine <55874001@qq.com>
2018-10-07 23:56:30 -0500

    Update Files

---
+++ 10-07/test.js
@@ -0,0 +1,25 @@
+// Update Files
+
+// fs.sppendFile()
+// var fs = require('fs');
+
+// fs.appendFile('mynewfile1.txt', ' Hello Kathy! ', function (err) { //先创建
+//     if (err) throw err;
+//     console.log('Created!');
+//   });
+// fs.appendFile('mynewfile1.txt', ' This is my text.', function (err) { //再更新
+//   if (err) throw err;
+//   console.log('Updated!');
+// });
+
+// fs.writeFile()
+var fs = require('fs');
+
+fs.writeFile('mynewfile3.txt', 'Hey Sweetie!', function (err) {
+    if (err) throw err;
+    console.log('Created!');
+  });
+fs.writeFile('mynewfile3.txt', 'Hi Moonpie!', function (err) {
+  if (err) throw err;
+  console.log('Replaced!');
+});
